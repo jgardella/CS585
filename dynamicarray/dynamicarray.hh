7087b5d11f7094bfe2110773c32e800290392be8
Jason Gardella <jgardell@stevens.edu>
2014-08-29 15:41:22 -0400

    Create header for DynamicArray.

---
+++ dynamicarray/dynamicarray.hh
@@ -0,0 +1,23 @@
+#ifndef _DYNAMICARRAY_HH_
+#define _DYNAMICARRAY_HH_
+
+// Dynamically sized array
+class DynamicArray<class T>
+{
+	public:
+		DynamicArray();
+		DynamicArray(unsigned int);
+		~DynamicArray();
+		pushFront(T);
+		pushBack(T);
+		swap(T, unsigned int, unsigned int);
+		insert(T, unsigned int);		
+		get(unsigned int);
+		length();
+		memoryUsage();
+		remove(unsigned int);
+	private:
+
+}
+
+#endif
