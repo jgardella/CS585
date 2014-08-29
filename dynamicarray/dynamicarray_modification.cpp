c21da2d7cc070d8595081d12354b0e3f693036b2
Jason Gardella <jgardell@stevens.edu>
2014-08-29 19:18:01 -0400

    Add comments to modification methods.

--- dynamicarray/dynamicarray_modification.cpp
+++ dynamicarray/dynamicarray_modification.cpp
@@ -1,13 +1,25 @@
 #include "dynamicarray.hh"
 
-void DynamicArray<T>::insert(T newElement, unsigned int index)
+// Inserts the element newElement at position index in the array.
+// Parameters:
+// T newElement - the new element to insert into the array
+// unsigned int index - the index at which to insert te new element
+// Returns the element that was in position index before insertion, or NULL if the insertion did not complete because the provided index was out of bounds.
+T DynamicArray<T>::insert(T newElement, unsigned int index)
 {
 	if(index < arrayLength)
 	{
+		T oldElement = this->firstElementPointer[index];
 		this->firstElementPointer[index] = newElement;
+		return oldElement;
 	}
+	return NULL;
 }
 
+// Swaps two elements in the array.
+// Parameters:
+// unsigned int indexOne - the index of the first item
+// unsigned int indexTwo - the index of the second item
 void DynamicArray<T>::swap(unsigned int indexOne, unsigned int indexTwo)
 {
 	if(indexOne < this->arrayLength && indexTwo < this->arrayLength)
@@ -18,14 +30,23 @@ void DynamicArray<T>::swap(unsigned int indexOne, unsigned int indexTwo)
 	}
 }
 
+// Gets the item at the specified index.
+// Parameters:
+// unsigned int index - the index of the item to retrieve
+// Returns the item at position index, or NULL if the provided index is out of bounds.
 T DynamicArray<T>::get(unsigned int index)
 {
 	if(index < this->arrayLength)
 	{
 		return this->firstElementPointer[index];
 	}
+	return NULL;
 }
 
+// Removes the item at the specified index.
+// Parameters:
+// unsigned int index - the index of the item to remove
+// Returns the item after removal, or NULL if the item could not be removed because the provided index was out of bounds.
 T DynamicArray<T>::remove(unsigned int index)
 {
 	int i;
@@ -49,4 +70,5 @@ T DynamicArray<T>::remove(unsigned int index)
 			firstElementPointer++; // move pointer to new front of array
 		}
 	}
+	return NULL;
 }
