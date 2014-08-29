c01ccbfe0f9185efeb7e703ec004746065bcaf5d
Jason Gardella <jgardell@stevens.edu>
2014-08-29 08:49:53 -0400

    Fix method returns in header.

--- jtest/jtest.hh
+++ jtest/jtest.hh
@@ -8,9 +8,9 @@ template <class T>
 class JTest
 {
 	public:
-		static bool testEquality(char*, T, T);
-		static bool testLessThan(char*, T, T);
-		static bool testGreaterThan(char*, T, T);
+		static void testEquality(char*, T, T);
+		static void testLessThan(char*, T, T);
+		static void testGreaterThan(char*, T, T);
 };
 
 #endif
