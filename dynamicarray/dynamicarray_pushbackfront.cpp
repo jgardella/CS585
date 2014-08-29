60d46fc9a8dfb2c30be5c627ec255ee6a08ab110
Jason Gardella <jgardell@stevens.edu>
2014-08-29 17:43:38 -0400

    Add comments to pushBack and pushFront.

--- dynamicarray/dynamicarray_pushbackfront.cpp
+++ dynamicarray/dynamicarray_pushbackfront.cpp
@@ -1,6 +1,7 @@
 #include "dynamicarray.hh"
 
-
+// Adds a new element onto the front end of the array.
+// T newElement - the new element to be added to the array
 DynamicArray<T>::pushFront(T newElement)
 {
 	this->firstElementPointer--;
@@ -8,6 +9,8 @@ DynamicArray<T>::pushFront(T newElement)
 	arrayLength++;
 }
 
+// Adds a new element onto the back end of the array.
+// T newElement - the new element to be added to the array
 DynamicArray<T>::pushBack(T newElement)
 {
 	firstElementPointer[arrayLength] = new Element;
