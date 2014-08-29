70063d91c05f3324ac4dafe583f8c12a915fd6e6
Jason Gardella <jgardell@stevens.edu>
2014-08-29 17:39:54 -0400

    Add comments to constructors.

--- dynamicarray/dynamicarray_constructors.cpp
+++ dynamicarray/dynamicarray_constructors.cpp
@@ -1,17 +1,20 @@
 #include "dynamicarray.hh"
 
+// Default constructor - allocates an array of size 10
 DynamicArray<T>::DynamicArray()
 {
-	this->firstElementPointer = new int[10];
+	this->firstElementPointer = new T[10];
 	this->arrayLength = 0;	
 }
 
+// Constructor which allocates an array of size 'preAllocatedLength'.
 DynamicArray<T>::DynamicArray(unsigned int preAllocatedLength)
 {
-	this->firstElementPointer = new int[preAllocatedLength];
+	this->firstElementPointer = new T[preAllocatedLength];
 	this->arrayLength = 0;
 }
 
+// Deconstructor, simply deletes array.
 DynamicArray<T>::~DynamicArray()
 {
 	delete firstElementPointer;
