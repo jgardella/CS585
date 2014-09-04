7fd98b06e63299f7c44faa5679d780cc78ba511d
Jason Gardella <jgardell@stevens.edu>
2014-09-04 00:53:20 -0400

    Add several helper methods to debug header.

--- debug/debug.hh
+++ debug/debug.hh
@@ -24,9 +24,9 @@ class Debug
 		void muteChannel(char* channel);
 		
 		// Channels
-		static const char* GAMEPLAY;
-		static const char* WARN;
-		static const char* ERROR;
+		static const char* GAMEPLAY = "GAMEPLAY";
+		static const char* WARN = "WARN";
+		static const char* ERROR = "ERROR";
 
 	private:
 		// Class is singleton, so the constructor, copy constructor, assignment operator, and deconstructor should be inaccessible.
@@ -35,6 +35,22 @@ class Debug
 		Debug& operator=(Debug const&){};
 		~Debug();
 		static Debug* instance;
+		
+
+		// Returns a timestamp representing the current time.
+		char* getTimestamp();
+
+		// Checks if a channel is valid.
+		// Parameters:
+		// char* channel - the channel to check the validity of
+		// Returns true if the channel is valid (i.e. defined as a static const char* in Debug), false if not.
+		bool isValidChannel(char* channel);
+
+		// Checks if a channel is muted.
+		// Parameters:
+		// char* channel - the channel to check the mute-status of
+		// Returns true if channel is muted, false if not.
+		bool isMuted(char* channel);
 };
 
 #endif
