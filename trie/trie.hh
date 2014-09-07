f067cd655ca4330cf67997f39f50d0ba6930bac6
Jason Gardella <jgardell@stevens.edu>
2014-09-07 17:28:11 -0400

    Add some comments to trie.

--- trie/trie.hh
+++ trie/trie.hh
@@ -5,6 +5,7 @@
 #include <string>
 #include "debug.hh"
 
+// Trie data structure implementation.
 template <class Value>
 class Trie
 {
@@ -17,11 +18,18 @@ class Trie
 			this->children = new DynamicArray<Trie*>();
 		}
 
+		// Adds a new value to the trie with the given key.
+		// Parameters:
+		// string key - the key to use to store the value
+		// Value value - the value to store
 		void add(std::string key, Value value)
 		{
 			addValue(key, value);
 		}
-
+		
+		// Returns the value in the array with the given key.
+		// Parameters:
+		// string key - the key to search for, must be a valid key which was previously added to the array
 		Value get(std::string key)
 		{
 			return findValue(key);
@@ -56,44 +64,53 @@ class Trie
 		{
 			Debug::log("TRIE", "findValue method entered with key " + key);
 			int i;
+			std::string firstLetter;
+			Trie* node;
 			if(key.compare("") != 0) // base case, if key is empty string, final node has been reached. return value.
 			{
-				std::string firstLetter = key.substr(0, 1);
+				firstLetter = key.substr(0, 1);
 				Debug::log("TRIE", "First letter of key is " + firstLetter);
 				// look through edges to find match with first letter.
 				for(i = 0; i < children->length(); i++)
 				{
-					Trie* node = children->get(i);
+					node = children->get(i);
 					if(node->key.compare(firstLetter) == 0)
 					{
 						Debug::log("TRIE", "Match found with key " + node->key);
-						return node->findValue(key.substr(1, std::string::npos)); // recursive call to follow edge to next node.
+						return node->findValue(key.substr(1, std::string::npos)); // recursive call to go to next node.
 					}
 				}
 				// key did not match any of nodes edges, so it is not in trie.
 			}
 			return value;
 		}
-
+		
+		// Recursively adds a new value to the trie using the given key.
+		// Parameters:
+		// string key - the key to use to store the value
+		// Value newValue - the value to add to the trie
 		void addValue(std::string key, Value newValue)
 		{
 			Debug::log("TRIE", "addValue method enter with key " + key);
 			int i;
 			std::string firstLetter;
-			if(key.compare("") != 0)
+			Trie* node;
+			if(key.compare("") != 0) // base case, if key is empty string, final node has been reached. set its value to the given value.
 			{
 				firstLetter = key.substr(0, 1);
 				Debug::log("TRIE", "First letter is " + firstLetter);
+				// look through children to find a match with the first letter of the key
 				for(i = 0; i < children->length(); i++)
 				{
-					Trie* node = children->get(i);
+					node = children->get(i);
 					if(node->key.compare(firstLetter) == 0)
 					{
 						Debug::log("TRIE", "Match found with key " + node->key);
-						node->addValue(key.substr(1, std::string::npos), newValue);
+						node->addValue(key.substr(1, std::string::npos), newValue); // recursive call to go to next node
 						return;
 					}
 				}
+				// branch did not exist and must be created.
 				Debug::log("TRIE", "Creating new node with key " + firstLetter);
 				Trie *newTrie = new Trie(firstLetter);
 				children->pushBack(newTrie);
