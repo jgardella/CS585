e16ffa26e31a3ffab5d12ce79961eced01fc9123
Jason Gardella <jgardell@stevens.edu>
2014-09-28 00:44:08 -0400

    Implement dispatcher.

--- include/dispatcher.hh
+++ include/dispatcher.hh
@@ -1,13 +1,19 @@
 #include "itickable.hh"
 #include "ilistenercallback.hh"
+#include "trie.hh"
+#include "dynamicarray.hh"
 
 class Dispatcher : public ITickable
 {
 	public:
 		
-		void addListener(std::string eventType, IListenerCallback callback);
+		void addListener(std::string eventType, IListenerCallback* callback);
 
-		void removeListener(std::string eventType, IListenerCallback callback);
+		void removeListener(std::string eventType, IListenerCallback* callback);
 
 		void dispatch(std::string eventType);
-}
+
+	private:
+		Trie<DynamicArray<IListenerCallback*>*> *callbacks;
+
+};
--- src/dispatcher.cpp
+++ src/dispatcher.cpp
@@ -1,16 +1,46 @@
 #include "dispatcher.hh"
 
-void Dispatcher::addListener(std::string eventType, IListenerCallback callback)
+void Dispatcher::addListener(std::string eventType, IListenerCallback* callback)
 {
-
+	DynamicArray<IListenerCallback*>* callbackArray = callbacks->get(eventType);
+	if(callbackArray == NULL)
+	{
+		callbackArray = new DynamicArray<IListenerCallback*>();
+		callbackArray->pushBack(callback);
+		callbacks->add(eventType, callbackArray);
+	}
+	else
+	{
+		callbackArray->pushBack(callback);
+	}
 }
 
-void Dispatcher::removeListener(std::string eventType, IListenerCallback callback)
+void Dispatcher::removeListener(std::string eventType, IListenerCallback* callback)
 {
-
+	unsigned int i;
+	DynamicArray<IListenerCallback*>* callbackArray = callbacks->get(eventType);
+	if(callbackArray != NULL)
+	{
+		for(i = 0; i < callbackArray->length(); i++)
+		{
+			if(*(callbackArray->get(i)) == callback)
+			{
+				callbackArray->remove(i);
+				break;
+			}
+		}		
+	}
 }
 
 void Dispatcher::dispatch(std::string eventType)
 {
-
+	unsigned int i;
+	DynamicArray<IListenerCallback*>* callbackArray = callbacks->get(eventType);
+	if(callbackArray != NULL)
+	{
+		for(i = 0; i < callbackArray->length(); i++)
+		{
+			(*callbackArray->get(i))->execute(*(new IEvent(eventType)));
+		}
+	}
 }
