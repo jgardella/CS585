ec437524b72a407f738328ed2cb16788d5f5fd65
Jason Gardella <jgardell@stevens.edu>
2014-09-29 23:27:57 -0400

    Add some more comments.

--- src/dispatcher.cpp
+++ src/dispatcher.cpp
@@ -4,6 +4,7 @@ void Dispatcher::addListener(std::string eventType, IListenerCallback* callback)
 {
 	Debug::getInstance()->log("DISPATCHER", "Adding listener of event type " + eventType + ".");
 	DynamicArray<IListenerCallback*>* callbackArray;
+	// if specified eventType does not exist in callback trie, create it and add callback
 	if(callbacks->get(eventType) == NULL)
 	{
 		Debug::getInstance()->log("DISPATCHER", "callbackArray is null, creating new array and adding event."); 
@@ -11,7 +12,7 @@ void Dispatcher::addListener(std::string eventType, IListenerCallback* callback)
 		callbackArray->pushBack(callback);
 		callbacks->add(eventType, callbackArray);
 	}
-	else
+	else // eventType already exists in callback trie, just add callback to array in trie
 	{
 		Debug::getInstance()->log("DISPATCHER", "callbackArray is not null, adding event."); 
 		callbackArray = *callbacks->get(eventType);
@@ -25,10 +26,12 @@ void Dispatcher::removeListener(std::string eventType, IListenerCallback* callba
 	Debug::getInstance()->log("DISPATCHER", "Removing listener of event type " + eventType + ".");
 	unsigned int i;
 	DynamicArray<IListenerCallback*>* callbackArray;
+	// check that events of the given type have previously been added
 	if(callbacks->get(eventType) != NULL)
 	{
 		callbackArray = *callbacks->get(eventType);
 		Debug::getInstance()->log("DISPATCHER", "callbackArray is not null, iterating through until listener is found.");
+		// iterate through callbacks until correct callback is found
 		for(i = 0; i < callbackArray->length(); i++)
 		{
 			if(*(callbackArray->get(i)) == callback)
@@ -44,6 +47,7 @@ void Dispatcher::removeListener(std::string eventType, IListenerCallback* callba
 void Dispatcher::dispatch(IEvent* event)
 {
 	Debug::getInstance()->log("DISPATCHER", "Dispatching event of type " + event->getType() + "."); 
+	// add event to event queue, will be dispatched during tick
 	eventQueue->enqueue(event);
 }
 
@@ -51,6 +55,7 @@ void Dispatcher::tick(float dt)
 {
 	unsigned int i;
 	IEvent* event;
+	// send all events in event queue to their listeners
 	while(!eventQueue->empty())
 	{
 		event = eventQueue->dequeue();
@@ -58,6 +63,7 @@ void Dispatcher::tick(float dt)
 		if(callbacks->get(event->getType()) != NULL)
 		{
 			callbackArray = *callbacks->get(event->getType());
+			// iterate through callbacks of given event type and execute them with the event
 			for(i = 0; i < callbackArray->length(); i++)
 			{
 				(*callbackArray->get(i))->execute(*event);
