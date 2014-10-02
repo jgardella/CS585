0570ec65ef1b01f0ea3a70da152885b0079e8ec0
Jason Gardella <jgardell@stevens.edu>
2014-10-01 20:34:57 -0400

    Fix dispatcher to do listener addition and removal at the end of the tick.

--- include/dispatcher.hh
+++ include/dispatcher.hh
@@ -7,6 +7,16 @@
 #include "queue.hh"
 #include "dynamicarray.hh"
 
+// Struct for holding listener modification data (eventType, callback) so it can be stored in one queue.
+typedef struct sListenerEvent
+{
+	IListenerCallback* callback;
+	std::string eventType;
+	
+	sListenerEvent(){ };
+	sListenerEvent(std::string type, IListenerCallback* cb) : callback(cb), eventType(type) {}
+} tListener;
+
 class Dispatcher : public ITickable
 {
 	public:
@@ -17,24 +27,24 @@ class Dispatcher : public ITickable
 		// Destructs the dispatcher.
 		~Dispatcher();
 
-		// Adds the given listener, associating it with the given event type.
+		// Queues the given listener, associated with the given event type, for addition at the end of this tick.
 		// Parameters:
 		// string eventType - the type of event that the listener is associated with
 		// IListenerCallback* callback - pointer to the listener
 		void addListener(std::string eventType, IListenerCallback* callback);
-
-		// Removes the given listener, which is associated with the given event type.
+		
+		// Queues the given listener, associated with the given event type, for removal at the end of this tick.
 		// Parameters:
-		// string eventType - the tyoe of event that the listener is associated with
+		// string eventType - the type of event that the listener is associated with
 		// IListenerCallback* callback - pointer to the listener to remove
 		void removeListener(std::string eventType, IListenerCallback* callback);
 
-		// Dispatches the given event.
+		// Queues the given event for dispatch at the end of this tick.
 		// Parameters:
 		// IEvent* event - the event to dispatch
 		void dispatch(IEvent* event);
 		
-		// Dispatches any events in the event queue to their associated listeners.
+		// Dispatches any events in the event queue, and adds and removes any listeners in their respective queues.
 		// Paramters:
 		// float dt - change in time
 		void tick(float dt);
@@ -44,6 +54,34 @@ class Dispatcher : public ITickable
 	private:
 		Trie<DynamicArray<IListenerCallback*>*> *callbacks;
 		Queue<IEvent*>* eventQueue;
+		Queue<sListenerEvent*>* addListenerQueue;
+		Queue<sListenerEvent*>* removeListenerQueue;
+		
+		// Dispatches all of the queued events.	
+		void processDeferredEvents();
+
+		// Removes all of the listeners queued for removal.
+		void processDeferredListenerRemovals();
+
+		// Adds all of the listeners queued for addition.
+		void processDeferredListenerAdditions();
+
+		// Dispatches the given deferred event.
+		// Parameters:
+		// IEvent* event - the deferred event to dispatch
+		void dispatchDeferredEvent(IEvent* event);
+
+		// Adds the given deferred listener, associating it with the given event type.
+		// Parameters:
+		// string eventType - the type of event that the listener is associated with
+		// IListenerCallback* callback - pointer to the deferred listener
+		void addDeferredListener(std::string eventType, IListenerCallback* callback);
+
+		// Removes the given deferred listener, which is associated with the given event type.
+		// Parameters:
+		// string eventType - the type of event that the listener is associated with
+		// IListenerCallback* callback - pointer to the deferred listener to remove
+		void removeDeferredListener(std::string eventType, IListenerCallback* callback);
 };
 
 #endif
--- src/dispatcher.cpp
+++ src/dispatcher.cpp
@@ -1,6 +1,88 @@
 #include "dispatcher.hh"
 
+Dispatcher::Dispatcher()
+{
+	Debug::getInstance()->log("DISPATCHER", "Constructing dispatcher.");
+	eventQueue = new Queue<IEvent*>();
+	addListenerQueue = new Queue<tListener*>();
+	removeListenerQueue = new Queue<tListener*>();
+	callbacks = new Trie<DynamicArray<IListenerCallback*>*>();
+}
+
+Dispatcher::~Dispatcher()
+{
+	delete eventQueue;
+	delete addListenerQueue;
+	delete removeListenerQueue;
+	delete callbacks;
+}
+
 void Dispatcher::addListener(std::string eventType, IListenerCallback* callback)
+{
+	addListenerQueue->enqueue(new tListener(eventType, callback));
+}
+
+void Dispatcher::removeListener(std::string eventType, IListenerCallback* callback)
+{
+	removeListenerQueue->enqueue(new tListener(eventType, callback));	
+}
+
+void Dispatcher::dispatch(IEvent* event)
+{
+	Debug::getInstance()->log("DISPATCHER", "Dispatching event of type " + event->getType() + "."); 
+	// add event to event queue, will be dispatched during tick
+	eventQueue->enqueue(event);
+}
+
+void Dispatcher::tick(float dt)
+{
+	processDeferredListenerAdditions();
+	processDeferredEvents();
+	processDeferredListenerRemovals();
+}
+
+void Dispatcher::processDeferredEvents()
+{
+	while(!eventQueue->empty())
+	{
+		dispatchDeferredEvent(eventQueue->dequeue());
+	}
+}
+
+void Dispatcher::processDeferredListenerRemovals()
+{
+	while(!removeListenerQueue->empty())
+	{
+		tListener* listener = removeListenerQueue->dequeue();
+		removeDeferredListener(listener->eventType, listener->callback);
+	}
+}
+
+void Dispatcher::processDeferredListenerAdditions()
+{
+	while(!addListenerQueue->empty())
+	{
+		tListener* listener = addListenerQueue->dequeue();
+		addDeferredListener(listener->eventType, listener->callback);
+	}
+}
+
+void Dispatcher::dispatchDeferredEvent(IEvent* event)
+{
+	unsigned int i;
+	DynamicArray<IListenerCallback*>* callbackArray;
+	if(callbacks->get(event->getType()) != NULL)
+	{
+		callbackArray = *callbacks->get(event->getType());
+		// iterate through callbacks of given event type and execute them with the event
+		for(i = 0; i < callbackArray->length(); i++)
+		{
+			(*callbackArray->get(i))->execute(event);
+		}
+	}
+}
+
+void Dispatcher::addDeferredListener(std::string eventType, IListenerCallback* callback)
 {
 	Debug::getInstance()->log("DISPATCHER", "Adding listener of event type " + eventType + ".");
 	DynamicArray<IListenerCallback*>* callbackArray;
@@ -21,7 +103,7 @@ void Dispatcher::addListener(std::string eventType, IListenerCallback* callback)
 	Debug::getInstance()->log("DISPATCHER", "Event added."); 
 }
 
-void Dispatcher::removeListener(std::string eventType, IListenerCallback* callback)
+void Dispatcher::removeDeferredListener(std::string eventType, IListenerCallback* callback)
 {
 	Debug::getInstance()->log("DISPATCHER", "Removing listener of event type " + eventType + ".");
 	unsigned int i;
@@ -42,47 +124,6 @@ void Dispatcher::removeListener(std::string eventType, IListenerCallback* callba
 			}
 		}		
 	}
-}
-
-void Dispatcher::dispatch(IEvent* event)
-{
-	Debug::getInstance()->log("DISPATCHER", "Dispatching event of type " + event->getType() + "."); 
-	// add event to event queue, will be dispatched during tick
-	eventQueue->enqueue(event);
-}
-
-void Dispatcher::tick(float dt)
-{
-	unsigned int i;
-	IEvent* event;
-	// send all events in event queue to their listeners
-	while(!eventQueue->empty())
-	{
-		event = eventQueue->dequeue();
-		DynamicArray<IListenerCallback*>* callbackArray;
-		if(callbacks->get(event->getType()) != NULL)
-		{
-			callbackArray = *callbacks->get(event->getType());
-			// iterate through callbacks of given event type and execute them with the event
-			for(i = 0; i < callbackArray->length(); i++)
-			{
-				(*callbackArray->get(i))->execute(event);
-			}
-		}
-	}
-}
-
-Dispatcher::Dispatcher()
-{
-	Debug::getInstance()->log("DISPATCHER", "Constructing dispatcher.");
-	eventQueue = new Queue<IEvent*>();
-	callbacks = new Trie<DynamicArray<IListenerCallback*>*>();
-}
-
-Dispatcher::~Dispatcher()
-{
-	delete eventQueue;
-	delete callbacks;
 }
 
 void Dispatcher::setGraph(ISceneGraph* graph)
