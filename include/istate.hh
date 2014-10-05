2cf2c86ab9924c86998fbcc71cdeaaebd04ae626
Jason Gardella <jgardell@stevens.edu>
2014-10-05 19:23:12 -0400

    Fix errors in state machine.

--- include/istate.hh
+++ include/istate.hh
@@ -1,6 +1,7 @@
 #ifndef _ISTATE_HH_
 #define _ISTATE_HH_
 
+#include "itickable.hh"
 #include "iactor.hh"
 
 class IState : public ITickable
--- include/statemachine.hh
+++ include/statemachine.hh
@@ -4,40 +4,54 @@
 #include "dynamicarray.hh"
 #include "trie.hh"
 #include "ievent.hh"
+#include "stateevent.hh"
 #include "itickable.hh"
 #include "ilistenercallback.hh"
 #include "istate.hh"
 
 class StateMachine : public ITickable
 {
-	// Listener callback for state transitions
-	class OnStateTransition : public IListenerCallback
-	{
-		public:
-			OnStateTransition() { }
-			OnMonsterKilled(const StateMachine* machine): stateMachine(machine){ }
-			virtual void execute(IEvent* event)
-			{
-				if(event->getType().compare("state") == 0)
-				{
-					StateEvent* stateEvent = (*StateEvent) event;
-					currentState = stateMap->get(stateEvent->getState());
-				}
-			}
-			
-
-		private:
-			const StateMachine* stateMachine;
-	} sceneTransitionListener(this);
 
 	public:
-		StateMachine(Trie<State>* map, Trie<float>* config) : stateMap(map), behavioralConfig(config), currentState(map->get("idle"));
+		StateMachine(Trie<IState*>* map, Trie<float>* config) : stateMap(map), behavioralConfig(config), currentState(*map->get("idle"))
+		{
+			onStateTransition.setInstance(this);
+		}
 		void tick(float dt);	
 
 	private:
-		IState* currentState;
 		Trie<IState*>* stateMap;
 		Trie<float>* behavioralConfig;
+		IState* currentState;
+		
+		void updateState(std::string stateString)
+		{
+			currentState = *stateMap->get(stateString);
+		}
+
+		// Listener callback for state transitions	
+		class OnStateTransition : public IListenerCallback
+		{
+			public:
+				OnStateTransition() { }
+				void setInstance(StateMachine* machine)
+				{
+					stateMachine = machine;
+				}
+				
+				virtual void execute(IEvent* event)
+				{
+					if(event->getType().compare("state") == 0)
+					{
+						StateEvent* stateEvent = (StateEvent*) event;
+						stateMachine->updateState(stateEvent->getState());
+					}
+				}
+				
+
+			private:
+				StateMachine* stateMachine;
+		} onStateTransition; 
 };
 
 #endif
