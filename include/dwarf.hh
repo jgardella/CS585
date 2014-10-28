9c3fefcef2a522a7db5482a303e1fd70bc80b6a1
Jason Gardella <jgardell@stevens.edu>
2014-10-27 21:00:25 -0400

    Declar orc and dwarf constructors.

--- include/dwarf.hh
+++ include/dwarf.hh
@@ -1,9 +1,18 @@
 #ifndef _DWARF_HH_
 #define _DWARF_HH_
 
-class Dwarf
+class Dwarf : public IActor
 {
+	public:
+		// Constructs a new dwarf.
+		Dwarf();
 
+		// Deconstructs the dwarf.
+		~Dwarf();
+
+	private:
+		unsigned int health;
+		const unsigned int MAX_ATTACK = 5;
 };
 
 #endif
--- include/orc.hh
+++ include/orc.hh
@@ -1,9 +1,14 @@
 #ifndef _ORC_HH_
 #define _ORC_HH_
 
-class Orc
+class Orc : public IActor
 {
-	
+	public:
+		// Constructs a new orc.
+		Orc();
+
+		// Deconstructs the orc.
+		~Orc();
 };
 
 #endif
